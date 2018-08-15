ed07b6254e940f8669c35a912709b2700df959ce
Roberto Allende <roberto@paperkite.co.nz>
2018-08-15 17:27:44 +1200

    Remove Unused Import.

--- library/src/main/java/com/suke/widget/SwitchButton.java
+++ library/src/main/java/com/suke/widget/SwitchButton.java
@@ -12,7 +12,6 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
